aa4d7eded1969a570985ab1679b21e69915c3eac
The Anh <Trinhtheanh789@Gmail.com>
2023-03-04 22:51:46 +0700

    Create test component, string interpolation, event binding

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css'],
+})
+export class TestComponent implements OnInit {
+  testString = 'String interpolation';
+  showData($event: any) {
+    console.log('button is clicked!');
+    if ($event) {
+      console.log("$event.target " ,$event.target);
+      console.log("$event.target.value " ,$event.target.value);
+    }
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
+}
