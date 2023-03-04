702fb48beb8990f4a56d3f8c50e6a2f61b4f00d1
The Anh <Trinhtheanh789@Gmail.com>
2023-03-04 21:58:31 +0700

    Add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // component selector name
   selector: 'app-root',
+  // template location
+  // can put in either raw code or url
   templateUrl: './app.component.html',
+  // style location
+  // can put raw style or url
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Peaceful Menu';
 }
