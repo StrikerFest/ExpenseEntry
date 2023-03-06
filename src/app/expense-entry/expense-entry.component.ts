079734074575dd18a5ee7ae3d7d0767cb98186a6
The Anh <Trinhtheanh789@Gmail.com>
2023-03-06 13:34:02 +0700

    Implement into project Add interface Add new interface object Add template

--- src/app/expense-entry/expense-entry.component.ts
+++ src/app/expense-entry/expense-entry.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpenseEntry } from './expense-entry';
 
 @Component({
   selector: 'app-expense-entry',
   templateUrl: './expense-entry.component.html',
-  styleUrls: ['./expense-entry.component.css','../app.component.css']
+  styleUrls: ['./expense-entry.component.css', '../app.component.css'],
 })
+
 export class ExpenseEntryComponent implements OnInit {
   title: string;
-  constructor() { }
+  expenseEntry: ExpenseEntry;
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.title = "Dis nut on ma chin";
+    this.title = "Expense Entry";
+    this.expenseEntry = {
+      id: 1,
+      item: "Pizza",
+      amount: 21,
+      category: "Food",
+      location: "Earth",
+      spendOn: new Date(2023, 3, 3, 10, 10, 10),
+      createdOn: new Date(2023, 3, 3, 10, 10, 10),
+    };
   }
-
 }
